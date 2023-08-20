77d4b5eb1b04a33c5e40dc2393eeeda5bac40b7d
navneet-chugh <navneetchugh15@gmail.com>
2023-08-20 20:53:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-const paragraph = document.getElementById('paragraph');
-
 function highlight() {
-    const boldWords = paragraph.querySelectorAll('strong');
+    const boldWords = document.querySelectorAll('p strong');
     for (const word of boldWords) {
         word.style.color = 'green';
     }
 }
 
 function return_normal() {
-    const boldWords = paragraph.querySelectorAll('strong');
+    const boldWords = document.querySelectorAll('p strong');
     for (const word of boldWords) {
         word.style.color = 'black';
     }
 }
 
-const highlightLink = document.querySelector('a'); // Change this selector according to your link's id or class
+const highlightLink = document.querySelector('a');
 highlightLink.addEventListener('mouseover', highlight);
 highlightLink.addEventListener('mouseout', return_normal);
