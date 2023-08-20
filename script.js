6668df358bd306434b40584163c14686f226843b
navneet-chugh <navneetchugh15@gmail.com>
2023-08-20 18:49:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+const paragraph = document.getElementById('paragraph');
+const highlightLink = document.getElementById('highlightLink');
 function highlight() {
     //Write your code here
-
+const boldWords = paragraph.querySelectorAll('strong');
+    for (const word of boldWords) {
+        word.style.color = 'green';
+    }
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+const boldWords = paragraph.querySelectorAll('strong');
+    for (const word of boldWords) {
+        word.style.color = 'black';
+    }
     
 }
+highlightLink.addEventListener('mouseover', highlight);
+highlightLink.addEventListener('mouseout', return_normal);
